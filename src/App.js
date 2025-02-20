631f38998daccb62541742f740e897b971356079
S Dev Aravind <devaravinds@gmail.com>
2025-02-20 22:16:24 +0530

    Cleanup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import './styles/App.css';
-import { RouterProvider } from 'react-router-dom';
-import { router } from './routes';
 import Home from './components/Home';
-
 
 const App = () => {
     return <Home />;
