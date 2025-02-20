d1a7eeb9f540ddf4f9712499957e6f8c1ec04ce0
S Dev Aravind <devaravinds@gmail.com>
2025-02-20 22:12:42 +0530

    Updated routing

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import React from 'react';
 import './styles/App.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
+import Home from './components/Home';
 
 
 const App = () => {
-    return (
-        <RouterProvider router={router} />
-    );
+    return <Home />;
 };
 
 export default App;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
 import Home from "../components/Home";
 import { PathName } from "../constants/pathname";
-import { createBrowserRouter } from 'react-router-dom';
+import { createHashRouter } from 'react-router-dom';
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     path: PathName.Home,
     element: <Home />,
